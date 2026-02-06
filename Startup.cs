3b9a1f66f303019660c8c262e82d62ac09b439e1
quocthinh503 <quocthinh503@gmail.com>
2026-02-06 08:53:43 +0700

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using inventory.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,7 +8,6 @@ namespace inventory
 {
     public class Startup
     {
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -23,23 +15,15 @@ namespace inventory
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddDbContext<BookContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BooksDB")));
-            
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-           
             app.UseDeveloperExceptionPage();
-            
 
-            //We don't want that...
-            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -53,3 +37,4 @@ namespace inventory
         }
     }
 }
+
